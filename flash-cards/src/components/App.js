0a9b0b0bc9e52da7ede1092b57ee78906a7adcf0
Jsparks1989 <jsparks19891@gmail.com>
2020-07-06 16:07:30 -0400

    Got the app running on Amplify. Now must make gradual changes to make sure it works and I can pinpoint any issues.

---
+++ flash-cards/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                Hello
+                {/* <StackOverflow2 /> */}
+            </div>
+        );
+    }
+}
+
+
+
+export default App;
--- flash-cards/src/index.js
+++ flash-cards/src/index.js
@@ -1,4 +1,19 @@
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+// import Amplify from "aws-amplify";
+// import awsExports from "./aws-exports";
+// Amplify.configure(awsExports);
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers/index';
+import App from './components/App';
+
+
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>, 
+    document.querySelector('#root')
+);
