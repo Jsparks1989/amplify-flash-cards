75eaff4b2c62943e3008ebefd7348d1a77133f8a
Jsparks1989 <jsparks19891@gmail.com>
2020-07-08 00:53:44 -0400

    Got the routing to work. Now I need to work on the drink test and the header for buttons

--- flash-cards/src/components/App.js
+++ flash-cards/src/components/App.js
@@ -1,37 +1,45 @@
 import React from 'react';
 import MenuCards from './MenuCards';
-import DrinkCards from './DinkCards';
-import { BrowserRouter, Route } from 'react-router-dom';
+import DrinkCards from './DrinkCards';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 
 
 
 
 class App extends React.Component {
 
-    menuCards = () => {
-        return(
-            <BrowserRouter>
-                <Route path='/menu' exact component={MenuCards} />
-            </BrowserRouter>
-        );
-    }
+    // menuTest = () => {
+    //     return(
+    //         <BrowserRouter>
+    //             <Route path='/menu' exact component={MenuCards} />
+    //         </BrowserRouter>
+    //     );
+    // }
 
-    drinkCards = () => {
-        return(
-            <BrowserRouter>
-                <Route path='/drinks' exact component={DrinkCards} />
-            </BrowserRouter>
-        );
-    }
+    // drinkTest = () => {
+    //     return(
+    //         <BrowserRouter>
+    //             <Route path='/drinks' exact component={DrinkCards} />
+    //         </BrowserRouter>
+    //     );
+    // }
 
     render() {
         return(
             <div>
-
-
-                <button onClick={this.menuCards()}>Menu test</button>
-                <button onClick={this.drinkCards()}>Drink test</button>
-                {/* <StackOverflow2 /> */}
+                <BrowserRouter>
+                    <NavLink to="/menu">
+                        <button>Menu test</button>
+                    </NavLink>
+                    <NavLink to="/drinks">
+                        <button>Drink test</button>
+                    </NavLink>
+                    <br />
+                    <Switch>
+                        <Route path="/drinks" exact component={DrinkCards} />
+                        <Route path="/menu" exact component={MenuCards} />
+                    </Switch>
+                </BrowserRouter>
             </div>
         );
     }
